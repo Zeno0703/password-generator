8c5e38bcea4299feaf7a684ea0f2c6c50ee08dc8
Zeno Vandenbulcke <zeno.vandenbulcke@student.kuleuven.be>
2022-08-27 21:55:48 +0200

    done

--- password-generator.py
+++ password-generator.py
@@ -11,12 +11,12 @@ root = tk.Tk()
 root.geometry("400x400")
 
 label = tk.Label(text="Password generator")
-label.pack(side="top")
+label.pack(side="top", fill="both")
 
 sliderVal = tk.IntVar()
 
 slider = tk.Scale(from_=8, to=16, orient="horizontal", variable=sliderVal)
-slider.pack()
+slider.pack(fill="both")
 
 val1 = tk.IntVar()
 val2 = tk.IntVar()
@@ -26,9 +26,9 @@ checkbox1 = tk.Checkbutton(text="Use capital letters", onvalue=1, offvalue=0, va
 checkbox2 = tk.Checkbutton(text="Use digits", onvalue=1, offvalue=0, variable=val2)
 checkbox3 = tk.Checkbutton(text="Use special characters", onvalue=1, offvalue=0, variable=val3)
 
-checkbox1.pack()
-checkbox2.pack()
-checkbox3.pack()
+checkbox1.pack(fill="both")
+checkbox2.pack(fill="both")
+checkbox3.pack(fill="both")
 
 
 def addTickedBoxes(standard):
@@ -52,21 +52,25 @@ def generatePassword():
 
     for i in range(passwordLength):
         alphabet = alphabetArray[random.randint(0, len(alphabetArray) - 1)]
-        password += alphabet[random.randint(0, len(alphabet) - 1)]
+        letter = alphabet[random.randint(0, len(alphabet) - 1)]
+
+        if password.count(letter) >= 2:
+            letter = alphabet[random.randint(0, len(alphabet) - 1)]
+
+        password += letter
 
     return password
 
 
 def displayPassword():
-    entry.delete(0, 17)
-    password = generatePassword()
-    entry.insert(0, password)
+    entry.delete(0, sliderVal.get() + 1)
+    entry.insert(0, generatePassword())
 
 
 button = tk.Button(text="Generate!", command=displayPassword)
-button.pack()
+button.pack(fill="both")
 
 entry = tk.Entry()
-entry.pack()
+entry.pack(fill="both")
 
 root.mainloop()
